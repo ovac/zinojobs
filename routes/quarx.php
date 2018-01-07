69e617cf0c141dcd70ce1f9eccc78528039ab0df
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-01-07 03:16:52 +0000

    Add admin routes

--- routes/quarx.php
+++ routes/quarx.php
@@ -27,92 +27,92 @@ Route::group(['namespace' => 'Quarx', 'middleware' => ['quarx-language', 'quarx-
     Route::get('events/event/{id}', 'EventsController@show');
 });
 
-// /*
-// |--------------------------------------------------------------------------
-// | Login/ Logout/ Password
-// |--------------------------------------------------------------------------
-//  */
-// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-// Route::post('login', 'Auth\LoginController@login');
-// Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+/*
+|--------------------------------------------------------------------------
+| Login/ Logout/ Password
+|--------------------------------------------------------------------------
+ */
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-// // Password Reset Routes...
-// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-// Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-// Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-// Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-// /*
-// |--------------------------------------------------------------------------
-// | Registration & Activation
-// |--------------------------------------------------------------------------
-//  */
-// Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-// Route::post('register', 'Auth\RegisterController@register');
+/*
+|--------------------------------------------------------------------------
+| Registration & Activation
+|--------------------------------------------------------------------------
+ */
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
 
-// Route::get('activate/token/{token}', 'Auth\ActivateController@activate');
-// Route::group(['middleware' => ['auth']], function () {
-//     Route::get('activate', 'Auth\ActivateController@showActivate');
-//     Route::get('activate/send-token', 'Auth\ActivateController@sendToken');
-// });
+Route::get('activate/token/{token}', 'Auth\ActivateController@activate');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('activate', 'Auth\ActivateController@showActivate');
+    Route::get('activate/send-token', 'Auth\ActivateController@sendToken');
+});
 
-// /*
-// |--------------------------------------------------------------------------
-// | Authenticated Routes
-// |--------------------------------------------------------------------------
-//  */
-// Route::group(['middleware' => ['auth']], function () {
+/*
+|--------------------------------------------------------------------------
+| Authenticated Routes
+|--------------------------------------------------------------------------
+ */
+Route::group(['middleware' => ['auth']], function () {
 
-//     /*
-//     |--------------------------------------------------------------------------
-//     | General
-//     |--------------------------------------------------------------------------
-//      */
+    /*
+    |--------------------------------------------------------------------------
+    | General
+    |--------------------------------------------------------------------------
+     */
 
-//     Route::get('/users/switch-back', 'Admin\UserController@switchUserBack');
+    Route::get('/users/switch-back', 'Admin\UserController@switchUserBack');
 
-//     /*
-//     |--------------------------------------------------------------------------
-//     | User
-//     |--------------------------------------------------------------------------
-//      */
+    /*
+    |--------------------------------------------------------------------------
+    | User
+    |--------------------------------------------------------------------------
+     */
 
-//     Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
-//         Route::get('settings', 'SettingsController@settings');
-//         Route::post('settings', 'SettingsController@update');
-//         Route::get('password', 'PasswordController@password');
-//         Route::post('password', 'PasswordController@update');
-//     });
+    Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
+        Route::get('settings', 'SettingsController@settings');
+        Route::post('settings', 'SettingsController@update');
+        Route::get('password', 'PasswordController@password');
+        Route::post('password', 'PasswordController@update');
+    });
 
-//     /*
-//     |--------------------------------------------------------------------------
-//     | Admin
-//     |--------------------------------------------------------------------------
-//      */
+    /*
+    |--------------------------------------------------------------------------
+    | Admin
+    |--------------------------------------------------------------------------
+     */
 
-//     Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
+    Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
 
-//         Route::get('dashboard', 'DashboardController@index');
+        Route::get('dashboard', 'DashboardController@index');
 
-//         /*
-//         |--------------------------------------------------------------------------
-//         | Users
-//         |--------------------------------------------------------------------------
-//          */
-//         Route::resource('users', 'UserController', ['except' => ['create', 'show']]);
-//         Route::post('users/search', 'UserController@search');
-//         Route::get('users/search', 'UserController@index');
-//         Route::get('users/invite', 'UserController@getInvite');
-//         Route::get('users/switch/{id}', 'UserController@switchToUser');
-//         Route::post('users/invite', 'UserController@postInvite');
+        /*
+        |--------------------------------------------------------------------------
+        | Users
+        |--------------------------------------------------------------------------
+         */
+        Route::resource('users', 'UserController', ['except' => ['create', 'show']]);
+        Route::post('users/search', 'UserController@search');
+        Route::get('users/search', 'UserController@index');
+        Route::get('users/invite', 'UserController@getInvite');
+        Route::get('users/switch/{id}', 'UserController@switchToUser');
+        Route::post('users/invite', 'UserController@postInvite');
 
-//         /*
-//         |--------------------------------------------------------------------------
-//         | Roles
-//         |--------------------------------------------------------------------------
-//          */
-//         Route::resource('roles', 'RoleController', ['except' => ['show']]);
-//         Route::post('roles/search', 'RoleController@search');
-//         Route::get('roles/search', 'RoleController@index');
-//     });
-// });
+        /*
+        |--------------------------------------------------------------------------
+        | Roles
+        |--------------------------------------------------------------------------
+         */
+        Route::resource('roles', 'RoleController', ['except' => ['show']]);
+        Route::post('roles/search', 'RoleController@search');
+        Route::get('roles/search', 'RoleController@index');
+    });
+});
